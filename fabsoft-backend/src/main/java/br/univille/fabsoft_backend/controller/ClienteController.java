0fedb5e3af43b4ee1163633663dac5f8e195377d
Walter Silvestre Coan <walter.coan@gmail.com>
2025-09-16 23:55:51 +0000

    WIP: validacao

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/controller/ClienteController.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/controller/ClienteController.java
@@ -1,12 +1,17 @@
 package br.univille.fabsoft_backend.controller;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.univille.fabsoft_backend.entity.Cliente;
 import br.univille.fabsoft_backend.service.ClienteService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
@@ -32,9 +38,17 @@ public class ClienteController {
             HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity<Cliente> save(@RequestBody Cliente cliente){
+    public ResponseEntity<Cliente> save(@Valid @RequestBody Cliente cliente, BindingResult result){
         if(cliente == null){
             return ResponseEntity.badRequest().build();
+        }
+        if (result.hasErrors()) {
+            HttpHeaders headers = new HttpHeaders();
+            String errorMessages = result.getAllErrors().stream()
+                    .map(error -> error.getDefaultMessage())
+                    .collect(Collectors.joining(" "));
+            headers.add("Erro", errorMessages);
+            return new ResponseEntity<Cliente>(cliente,headers,HttpStatus.BAD_REQUEST);
         }
         if(cliente.getId() == 0){
             cliente = service.save(cliente);
